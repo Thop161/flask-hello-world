94df2a7ef65767d03ebba84e4d26bc3e05981144
Tyler Hopkins <tylerhopkins@Tylers-MBP.lan>
2024-07-25 10:25:50 -0400

    Adding my record to the table

--- app.py
+++ app.py
@@ -40,7 +40,8 @@ def inserting():
         ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
         ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
         ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
-        ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+        ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2),
+        ('Tyler', 'Hopkins', 'CU Boulder', 'Louisville', 'CSPB 3308');
     ''')
     conn.commit()
     conn.close()
